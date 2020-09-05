3f7287bb8b0b9402e86c73d8f4d6f9b66c470b32
DragonCoder <rrandall_2001@yahoo.com>
2020-09-05 12:29:27 -0700

    Add files via upload

---
+++ calc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+  int x, y;
+  int sum;
+  cout << "Enter a number: ";
+  cin  >> x;
+  cout << "Enter another number: ";
+  cin  >> y;
+  sum  = x + y;
+  cout << "The sum is: " << sum;
+  return 0;
+}
